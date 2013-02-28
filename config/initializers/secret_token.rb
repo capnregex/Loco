e7b6a53cb369e7d6e6c69d2b6c808d7f870b0f4f
Robert Ferney <capnregex@gmail.com>
2013-02-28 14:07:57 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loco::Application.config.secret_token = 'bb6c13d846b9c861573418957eee04d4f6f92eb530bd18d4f6dd078dc95224b97da91ada7b378ad3dde1ff83531c2c56931e0fe4255c7e791c247e82e58e2e95'
